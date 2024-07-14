0070d44e2df3b7ceb245043300f083492132840b
vanshika704 <sharmavanshi704@gmail.com>
2024-07-14 13:11:31 +0530

    nananananannanana

--- firstserver.js
+++ firstserver.js
@@ -1,2 +1,8 @@
 const fs = require("fs");
-const http = require ("http");// required for network requests
+const http = require("http");// required for network requests
+
+//////////////////////////////////////////
+//Server
+http.createServer((req, res) => {
+    res.end("aami tumhike bhalu bhashu");
+});
