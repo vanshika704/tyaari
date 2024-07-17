7b45b4e8e7e1be4b626a87dc4b2ca681d9552642
vanshika704 <sharmavanshi704@gmail.com>
2024-07-17 22:18:37 +0530

    npm init

--- firstserver.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const http = require("http");// required for network requests
-
-//////////////////////////////////////////
-//Server
-http.createServer((req, res) => {
-    res.end("aami tumhike bhalu bhashu");
-});
