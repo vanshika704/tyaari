5006fc972e58b66979cc0639f4998b7348ba7c28
vanshika704 <sharmavanshi704@gmail.com>
2024-07-17 22:38:23 +0530

    aaha tamatar bde majedaar

--- server.js
+++ server.js
@@ -3,3 +3,7 @@
 }
 var result = add(2,7);
 console.log(result);
+// now if you want to run a function itself , then go for self called function
+(function name (){
+console.log("ahha tamatar bde majedaar waah tamatar bde majedaar");
+})();
