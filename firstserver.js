6433696854d9b542464d9eae9cb4c05f53a430a6
vanshika704 <sharmavanshi704@gmail.com>
2024-08-09 20:52:01 +0530

    .

--- firstserver.js
+++ firstserver.js
@@ -1,6 +1,6 @@
-let http = require("http");
+let http = require("http"); // http naam ka ek variable bnaya , jisme require rkek http ko bulaya
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('This is an example of a Node.js web-based application.\n');
-}).listen(5000, () => console.log('Server running at http://localhost:5000'));
+http.createServer(function (req, res) { // http ka ek function ,  create server use kiya or usme ek function define kiya jiske pass resolution or requests hongi
+    res.writeHead(200, {'Content-Type': 'text/plain'}); // abbhi res .writeHead ka use kiya or usme btaya ki content type text hai plain uska
+    res.end('This is an example of a Node.js web-based application.\n'); // res.end krke server pr type kiya
+}).listen(5000, () => console.log('Server running at http://localhost:5000')); // lsiten laya jisme 5000 server pr  arrow function bnaya , jo btayeg ki is server pr chlega
