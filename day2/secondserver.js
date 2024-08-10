cf3309f5db32534631db397c2a43e107a25f790c
vanshika704 <sharmavanshi704@gmail.com>
2024-08-10 14:15:04 +0530

    .

--- day2/secondserver.js
+++ day2/secondserver.js
@@ -1,5 +1,24 @@
-let http = require ('http');
-http.createServer(function (req , res){
-    res.writeHead(200, {'Content-Type': 'text/plain'}); 
-    res.end ("Compare your Entries");
-}).listen(8080 , () => console.log('server is running  at  http://localhost:8080'))
+const http = require('http');
+const assert = require('assert').strict;
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    let responseMessage = '';
+
+    try {
+        assert.deepStrictEqual({ a: '5' }, { a: '5' });
+        responseMessage += "First Assertion Passed\n";
+    } catch (error) {
+        responseMessage += `Error in First Assertion: ${error.message}\n`;
+    }
+
+    try {
+        assert.deepStrictEqual({ a: 5 }, { a: '5' });
+        responseMessage += "Second Assertion Passed";
+    } catch (error) {
+        responseMessage += `Error in Second Assertion: ${error.message}`;
+    }
+
+    res.end(responseMessage);
+}).listen(8080, () => console.log('Server is running at http://localhost:8080'));
