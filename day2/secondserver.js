c9387a64cb5c72fc11f6f32cdce4851aa37e046d
vanshika704 <sharmavanshi704@gmail.com>
2024-08-10 15:32:12 +0530

    assert.deepStrictEquals

--- day2/secondserver.js
+++ day2/secondserver.js
@@ -4,8 +4,8 @@ const assert = require('assert').strict;
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     try {
-        assert.deepStrictEqual({ a: '5' }, { a: '5' });
-        res.end("No Error Occurred");
+        assert.deepStrictEqual({ a: '5' }, { a: '5' }); // using assert.deepStrictEqual
+        res.end("Yes, '5' equals '5' ");
     } catch (error) {
         res.writeHead(500, {'Content-Type': 'text/plain'}); // Sending 500 status code for errors
         res.end(`Error: ${error.message}`); // Use template literal to include error message
