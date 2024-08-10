8f1e637fb48ca5cd3955bc74ed522ccb09e66026
vanshika704 <sharmavanshi704@gmail.com>
2024-08-10 16:44:54 +0530

    routing

--- day2/secondserver.js
+++ day2/secondserver.js
@@ -3,6 +3,16 @@ const assert = require('assert').strict;
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/assertion') {
+        res.end("This is the example of assertion");
+        return;
+    } else if (pathName === '/nexttopic') {
+        res.end("This is the example of nexttopic");
+        return;
+    }
+
     try {
         assert.deepStrictEqual({ a: '5' }, { a: '5' }); // using assert.deepStrictEqual
         res.end("Yes, '5' equals '5' ");
@@ -11,5 +21,4 @@ http.createServer((req, res) => {
         res.end(`Error: ${error.message}`); // Use template literal to include error message
     }
 
-    
 }).listen(8080, () => console.log('Server is running at http://localhost:8080'));
