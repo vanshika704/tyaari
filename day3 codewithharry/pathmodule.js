ab3e557b5111303e2281adfdee2b04c4109d1a16
vanshika704 <sharmavanshi704@gmail.com>
2024-08-11 21:06:03 +0530

    .

--- day3 codewithharry/pathmodule.js
+++ day3 codewithharry/pathmodule.js
@@ -1,2 +1,5 @@
 const path = require('path');
  console.log (path.basename('c:\\temp\\myfile.html'));
+ console.log (path.dirname('c:\\temp\\myfile.html'));
+ console.log (path.extname('c:\\temp\\myfile.html'));
+ console.log (path.extname(__filename));
