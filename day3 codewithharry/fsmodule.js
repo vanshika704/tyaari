713a2a02f01b26db47d802546fe5732dd8e0b6bb
vanshika704 <sharmavanshi704@gmail.com>
2024-08-11 22:38:57 +0530

    aaha tmatar bde majedaar

--- day3 codewithharry/fsmodule.js
+++ day3 codewithharry/fsmodule.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
-fs.readFile('file.txt', 'utf-8' 
-    , (err,data) =>{
-    console.log(err,data)
-})
+// fs.readFile('file.txt', 'utf-8' 
+//     , (err,data) =>{
+//     console.log(err,data)
+// })
 const a = fs.readFileSync('file.txt') 
     
     console.log(a.toString());
 
+    // const b = fs.writeFile('file2.txt', "aaha tamatar bde majedaar ", () => {
+    //     console.log("written file");
+    // });
+const b1 = fs.writeFileSync('file2.txt', "waah tamatar bde majedar")
+console.log(b1);
+console.log("likhdi file");
 
 console.log("finished reading file");
