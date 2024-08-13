b278c534cbbe43207a796f6aa1bf4a4975edc1ad
vanshika704 <sharmavanshi704@gmail.com>
2024-08-13 21:33:05 +0530

    done

--- day4/website.js
+++ day4/website.js
@@ -1,13 +1,20 @@
 const http = require('http');
-http.createServer(function (req, res){
-    res.writeHead(200, {'Content-type': 'text/plain'});
-    if (req.url == '/'){
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-type': 'text/html'});
+
+    if (req.url == '/') {
         res.end("aagye firse muh uthake?");
-    }else if(req.url =='/about'){
+    } else if (req.url == '/about') {
         res.end("Cheen tapak dam dam");
-    }else{res.statusCode = 404;
+    } else if (req.url == '/hello') {
+        const data = fs.readFileSync('index.html', 'utf-8');
+        res.end(data);
+    } else {
+        res.statusCode = 404;
         res.end('not found');
     }
-
-   
-    }).listen(5000,()=>{console.log('the server is running at http://localhost:5000')});
+}).listen(5000, () => {
+    console.log('The server is running at http://localhost:5000');
+});
