a9ccc79111895c10cbcb5e9765f7962185a79bab
vanshika704 <sharmavanshi704@gmail.com>
2024-08-14 21:53:42 +0530

    express server

--- day5/index.js
+++ day5/index.js
@@ -2,7 +2,16 @@
 import express from "express";
 
 const app = express();
-
+// Get 
+// Post 
+//Patch 
+//Delete
+app.get("/",(req, res)=>{res.status(200).json({message: "This is json response "})
+    res.end("Aalo kachalu beta kahan gye the");
+})
+app.get("/hello", (req, res)=>{
+    res.send("Hello World");
+})
 app.listen(3000, () => {
-    console.log("Server is working");
+    console.log("Server is working at http://localhost:3000");
 });
