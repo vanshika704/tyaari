7f3a712021cfb94a0157a9e6a9e3edc15bc56449
vanshika704 <sharmavanshi704@gmail.com>
2024-08-20 22:26:59 +0530

    done html

--- day7/starter/index.js
+++ day7/starter/index.js
@@ -5,14 +5,16 @@ const http = require('http');
 const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
-const replaceTemplate = (temp, product) =>{let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+const replaceTemplate = (temp, product) =>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
     output = output.replace(/{%IMAGE%}/g, product.image);
     output = output.replace(/{%PRICE%}/g, product.price);
     output = output.replace(/{%FROM%}/g, product.from);
     output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
     output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
     output = output.replace(/{%ID%}/g, product.id);
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+   if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
    return output;
 }
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
