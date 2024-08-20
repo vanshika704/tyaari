b2f6395d491cc22e85723778c87fbe8453e60a9b
vanshika704 <sharmavanshi704@gmail.com>
2024-08-20 20:48:43 +0530

    .

--- day7/final/index.js
+++ day7/final/index.js
@@ -1,7 +1,7 @@
 // const fs = require('fs');
 // const textln = fs.readFileSync('../../starter/txt/input.txt', 'utf-8');
 // console.log(textln);
-
+const temp = fs.readfileSync('${__dirname}/dev-data/data.json', 'utf-8')
 const data = fs.readfileSync('${__dirname}/dev-data/data.json', 'utf-8');
 const dataObj = JSON.parse(data);
 
@@ -10,13 +10,16 @@ const server = http.createServer((req, res) =>{
     if (pathname === '/' || pathname ==='/overview'){
         res.end('This is the OVERVIEW');
     }
-    else if (pathname === '/product') {
+    else if (pathname === '/product') { 
         res.end('This is Product');
     }
+
+    //API
     else if (pathname === '/api'){
         res.writehead(200, {'Content-type' : 'application.json'});
         res.end(data);
     }
+    //NOT FOUND
     else {
         res.writeHead(404 , {
             'Content-type':'text/html',
