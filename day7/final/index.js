ac01cbe37c65bb6accf3a3e384e7c3cada8f2f18
vanshika704 <sharmavanshi704@gmail.com>
2024-09-04 21:11:03 +0530

    .

--- day7/final/index.js
+++ day7/final/index.js
@@ -1,34 +1,96 @@
-// const fs = require('fs');
-// const textln = fs.readFileSync('../../starter/txt/input.txt', 'utf-8');
-// console.log(textln);
-const tempOverview = fs.readfileSync('${__dirname}/templates/overview.html', 'utf-8')
-const tempCard = fs.readfileSync('${__dirname}/templates/card.html', 'utf-8')
-const tempProduct = fs.readfileSync('${__dirname}/templates/product.html', 'utf-8')
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 
-const data = fs.readfileSync('${__dirname}/dev-data/data.json', 'utf-8');
+/////////////////////////////////
+// FILES
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!');
+
+// Non-blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR! 💥');
+
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been written 😁');
+//       })
+//     });
+//   });
+// });
+// console.log('Will read file!');
+
+/////////////////////////////////
+// SERVER
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const server = http.createServer((req, res) =>{
-    const pathname = req.url;
-    if (pathname === '/' || pathname ==='/overview'){
-        res.writehead(200, {'Content-type' : 'text/html'});
-        res.end(tempOverview);
-    }
-    else if (pathname === '/product') { 
-        res.end('This is Product');
-    }
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
-    //API
-    else if (pathname === '/api'){
-        res.writehead(200, {'Content-type' : 'application.json'});
-        res.end(data);
-    }
-    //NOT FOUND
-    else {
-        res.writeHead(404 , {
-            'Content-type':'text/html',
-            'my-ownheader' :'hellllo guyz'
-        })
-        res.end('<h1>Page not found </h1>')
-    }
-})
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+    // Product page
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // API
+  } else if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    });
+    res.end(data);
+
+    // Not found
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
