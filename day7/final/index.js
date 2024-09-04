a71686e6479ae29784b6acf95d37e5427670c50d
vanshika704 <sharmavanshi704@gmail.com>
2024-09-04 21:13:55 +0530

    done with first folder ,

--- day7/final/index.js
+++ day7/final/index.js
@@ -92,5 +92,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000');
+  console.log('Port is working on http://localhost:8000');
 });
