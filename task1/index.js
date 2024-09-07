ec7bf5e3a07ee50c3b24696d9d2a9a2672adb763
vanshika704 <sharmavanshi704@gmail.com>
2024-09-07 10:25:38 +0530

    .

--- task1/index.js
+++ task1/index.js
@@ -1,18 +1,28 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const path = require('path');
 
-http.createServer((req , res) =>{
-    res.writeHead(200 , {'Content-type': 'text/plain'});
-    const pathName = req.url;
 
-if (pathName === "/"){
-    res.end ("first page");
-    return;
-}
-else if (pathName ==='/studentid'){
-    res.end("this is student id page ");
-}
-}).listen(3000, ()=> {
-    console.log("http://localhost:3000");
-})
+const adminPanel = fs.readFileSync(path.join(__dirname, 'firstpage.html'), 'utf-8');
+
+http.createServer((req, res) => {
+   
+    const pathName = url.parse(req.url, true).pathname;
+
+    if (pathName === "/") {
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(adminPanel);
+    } else if (pathName === '/studentid') {
+      
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end("This is the student ID page");
+    } else {
+       
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end("404 - Page not found");
+    }
+}).listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
