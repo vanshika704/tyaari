232148477367847a5f8e1123128a66917b67cc40
vanshika704 <sharmavanshi704@gmail.com>
2024-10-03 02:13:13 +0530

    done

---
+++ gdsctask2/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv"; 
+
+dotenv.config(); 
+
+const app = express();
+const PORT = process.env.PORT ;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`); 
+});
+
+app.get('/', (req, res) => {
+  res.send("Hello from the server");
+});
