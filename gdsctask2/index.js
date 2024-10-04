e7f53d60b0dd9084950ffa579cdeb4cbdbe521fc
vanshika704 <sharmavanshi704@gmail.com>
2024-10-04 21:47:36 +0530

    uhjknm

--- gdsctask2/index.js
+++ gdsctask2/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv"; 
-
+import { connectmongo } from "./db/db.js";
 dotenv.config(); 
 
 const app = express();
@@ -9,7 +9,8 @@ const PORT = process.env.PORT ;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`); 
 });
-
+connectmongo();
 app.get('/', (req, res) => {
   res.send("Hello from the server");
 });
+
