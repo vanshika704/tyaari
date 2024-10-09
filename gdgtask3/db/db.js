6dddc4c3dd1116adf09c8b459d4cbada2be40dbd
vanshika704 <sharmavanshi704@gmail.com>
2024-10-09 18:27:54 +0530

    oedlfgv

--- gdgtask3/db/db.js
+++ gdgtask3/db/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv"; 
+ 
 export const connectmongo = async ()=>{
     try {
         const instance = await mongoose.connect(process.env.MONGO_URI);
--- gdgtask3/index.js
+++ gdgtask3/index.js
@@ -5,4 +5,10 @@ dotenv.config();
  const app = express();
  const PORT = process.env.PORT || 3000;
 
- 
+ app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`); 
+  });
+  connectmongo();
+  app.get('/', (req, res) => {
+    res.send("Hello from the server");
+  });
