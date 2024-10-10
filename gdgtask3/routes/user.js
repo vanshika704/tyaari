9d3472240a62c315bae817e0af926c0d7222c09f
vanshika704 <sharmavanshi704@gmail.com>
2024-10-10 19:12:23 +0530

    user.js ka comments

--- gdgtask3/routes/user.js
+++ gdgtask3/routes/user.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from "express"; // express ko import kiyab 
 import { AhhaTamatar } from "../controllers/user.js";  // Ensure the path is correct
 
-const router = express.Router();
+const router = express.Router(); // router use kiya express ka
 
 
-router.route("/say-hello").get(AhhaTamatar);
+router.route("/say-hello").get(AhhaTamatar); // router.route route btara h ,orr .get uspe jaake function  use krna
 
 export default router;
