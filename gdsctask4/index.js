6907e1b28816ec02424bc3f6a789e7484f1a6c0c
vanshika704 <sharmavanshi704@gmail.com>
2024-10-25 23:00:46 +0530

    hj

--- gdsctask4/index.js
+++ gdsctask4/index.js
@@ -1,17 +1,17 @@
-import express from "express";
-import connectMongo from "./db/db.js";
-import dotenv from "dotenv";
-import userRouter from "./routes/user.js";
-dotenv.config();
-const app = express();
+import express from "express"; // express import  kro bhyi
+import connectMongo from "./db/db.js"; // db.js ko bulaya 
+import dotenv from "dotenv"; //dotenv 
+import userRouter from "./routes/user.js"; //userrouter
+dotenv.config(); // dotenv bulaya 
+const app = express();// express
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!"); // app.get krke hello wrld
 });
 
-app.use("/api/v1/user", userRouter);
+app.use("/api/v1/user", userRouter);// path bnaya 
 
 app.listen(3000, () => {
-  connectMongo();
+  connectMongo(); // app.listen krke server on krdiya
   console.log("Server is running on port 3000");
 });
