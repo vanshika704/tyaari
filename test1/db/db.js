1d94bce94102f220db52fdbd0cbe18d969313f23
vanshika704 <sharmavanshi704@gmail.com>
2024-10-27 13:33:40 +0530

    dd

--- test1/db/db.js
+++ test1/db/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
-const connectMongo = async() =>{
+const connectMongo = async()=>{
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("Mongodb connected ");
+    console.log("mongodb connected");
 }
 export default connectMongo;
--- test1/modals/user.js
+++ test1/modals/user.js
@@ -1,1 +1,25 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"; // import mongoose from mongoose
+
+const userSchema = new mongoose.Schema( // mongoose.schema
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "Email should be unique"],
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema); // mongoose.model ko export kra
+
+// users
