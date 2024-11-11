8a531083a7a4dc7e71251f07ac526ec5175d712f
vanshika704 <sharmavanshi704@gmail.com>
2024-11-11 22:21:21 +0530

    server complete

--- practice1/index.js
+++ practice1/index.js
@@ -2,3 +2,11 @@ import express from "express"
 import dotenv from "dotenv"
 
 const app = express();
+const PORT = 5050;
+app.get("/" ,(req, res)=>{
+    res.send("aaha tamatar bde majedar")
+})
+
+app.listen(PORT, ()=>{
+    console.log("http://localhost:5050");
+})
