fba37f8808716b5b66a52adf99f8efba919239db
vanshika704 <sharmavanshi704@gmail.com>
2024-11-11 22:17:54 +0530

    ..

--- test2/index.js
+++ test2/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT||3000;
-
+app.use("api/vi/users" , Registers)
 app.listen(PORT, (req,res)=>{
     console.log(`https://localhost${PORT}`)
 })
