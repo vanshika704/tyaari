639fd1dff1a27a4deeac82d596c727d1a2f2cd9d
vanshika704 <sharmavanshi704@gmail.com>
2024-11-13 19:30:35 +0530

    huehuehue

--- practice1/index.js
+++ practice1/index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import userpage from "./routes/user.js"; // Importing the user route
 
-const app = express();
-const PORT = 5050;
+const app = express();// express
+const PORT = 5050;// port setup
 
 app.get("/", (req, res) => {
-    res.send("aaha tamatar bde majedar");
+    res.send("aaha tamatar bde majedar");// aap.get krke phla route bnaya 
 });
 
 
-app.use('/tamatar', userpage);
+app.use('/tamatar', userpage);// app.use krkke doosra route access kiya 
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
--- practice1/routes/user.js
+++ practice1/routes/user.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 
-router.get("/api/v1/users", (req, res) => {
+router.get("/api/v1/users", (req, res) => { // router.get krkr yha route set kiya , req, res add krke 
     res.send("waah tamatar bde majedaar");
 });
 
