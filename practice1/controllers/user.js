37cf9a3e1a54819c2ba762ef95c917250954895f
vanshika704 <sharmavanshi704@gmail.com>
2024-11-18 16:45:07 +0530

    ..

--- practice1/controllers/user.js
+++ practice1/controllers/user.js
@@ -3,7 +3,7 @@ const User = require('./models/User'); // user modal ko import kiya
 const createUser = async () => { // create user name ke ek const me sirf call lgai h 
     const user = new User({
         name: 'John Doe',
-        email: 'john.doe@example.com',
+        email: 'john.doe@example.com', // or values pass ki h
         password: 'securepassword',
     });
 
