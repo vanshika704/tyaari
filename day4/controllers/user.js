408da7eca428f7eed8a095bf9186fa1aa0d29499
vanshika704 <sharmavanshi704@gmail.com>
2024-12-24 19:24:55 +0530

    users

--- day4/controllers/user.js
+++ day4/controllers/user.js
@@ -1,38 +1,3 @@
-// import { User } from "../models/user.js";
-// import bcrypt from "bcryptjs";
-// export const register = async (req, res) => {
-//   try {
-//     //1. get the data from the request body
-//     const { username, email, password } = req.body;
-//     // 2. check if user already exists
-//     const existingUser = await User.findOne({ email });
-//     if (existingUser) {
-//       return res.status(409).json({ message: "User already exists" });
-//     }
-//     // 3. hash the passowrd
-//     const salt = bcrypt.genSaltSync(10);
-
-//     const hashedPassword = bcrypt.hashSync(password, salt);
-
-//     // 4. create a user
-//     const user = await User.create({
-//       username,
-//       email,
-//       password: hashedPassword,
-//     });
-//     // 5. send a response
-//     res.status(201).json({ message: "User created successfully", user });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-
-
-
-
-
 
 import { User } from "../models/user.js";
 import bcrypt from "bcryptjs";
