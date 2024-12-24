deb34e549396630d55a8f46ec0d5106e6966b01c
vanshika704 <sharmavanshi704@gmail.com>
2024-12-24 19:25:56 +0530

    ..

--- day4/models/user.js
+++ day4/models/user.js
@@ -1,28 +1,3 @@
-// import mongoose from "mongoose"
-
-// const UserSchema = mongoose.Schema[{
-//    username:{
-//     type: String,
-//     required: true,
-//    },
-//    email:{
-//     type: String,
-//     required: true,
-//    },
-//    password:{
-//     type: String,
-//     required: true,
-//     minlength: 6 ,
-//    },
-//    posts:[{
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Post",
-//    }]
-// },{Timestamp: true}];
-
-// export const User = mongoose.model("User", UserSchema);
-
-
 
 import mongoose from "mongoose";
 
